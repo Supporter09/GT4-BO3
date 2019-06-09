78c789c2c0de03cd0e61868a762e45d02d0be834
unknown <maivannhatminh2005@gmail.com>
2019-06-09 12:06:14 +0700

    first commit

---
+++ section1.py
@@ -0,0 +1,18 @@
+# print("Hello World  sssss!!!")
+# Input=input('Nhap cai gi vao cho don gian :')
+# number=int(1)
+# print(int(number))
+
+print('Tinh dien tich tam giac')
+day=float(input('Nhap do dai day cua tam giac:'))
+cao=float(input('Nhap chieu cao cua tam giac:'))
+S_tamgiac= day*cao*0.5
+
+print("Dien tich hinh tron:",S_tamgiac)
+
+print('--------------------------------')
+
+print('Tinh dien tich hinh tron! theo cong thuc pi*d')
+duongkinh=float(input('Nhap duong kinh:'))
+S_tron=3.14*duongkinh
+print(S_tron)
